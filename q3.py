e33f1e3f087806b9951bbc6755f85c16e9e69d65
Jagannath09255 <84753861+Jagannath09255@users.noreply.github.com>
2024-08-07 13:11:04 +0530

    Add files via upload

---
+++ q3.py
@@ -0,0 +1,14 @@
+# Declaring variables to store personal information
+# Assign the name "Claude" to the variable 'name'
+name = "Jagannath Mahata"
+# Assign the age 1 to the variable 'age'
+age = 22
+# Assign the hobby "Learning" to the variable 'favorite_hobby'
+favorite_hobby = "Learning"
+# Printing the variables to display the information
+# Print the name with a descriptive label
+print("Name:", name)
+# Print the age with a descriptive label
+print("Age:", age)
+# Print the favorite hobby with a descriptive label
+print("Favorite hobby:", favorite_hobby)
